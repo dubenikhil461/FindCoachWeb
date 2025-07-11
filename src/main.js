84277e477acea8739a7c0b1db8fbfe82e34d0a9b
Nikhil Dubey <nikhildubey461@gmail.com>
2025-07-11 17:12:12 +0530

    feat: integrate Pinia for state management in main application setup

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import './assets/main.css'
 import { plugin, defaultConfig } from '@formkit/vue'
 import router from './router/routes.js'
 import { Quasar } from 'quasar'
-
+import { createPinia } from 'pinia'
 import 'quasar/src/css/index.sass'
 import '@quasar/extras/material-icons/material-icons.css'
 
 const app = createApp(App)
-
+const pinia = createPinia()
+app.use(pinia)
 app.use(Quasar, {
   plugins: {}
 })
